96345a4db9df03200c7e5cf9155e8cbf9b29ebbd
teklamandaria <teklamandaria13@gmail.com>
2024-07-28 02:24:45 -0800

    first commit

---
+++ Day 01/Homework/lesson.py
@@ -0,0 +1,7 @@
+name = 'tekla'
+
+surname = 'mandaria'
+
+age = '11'
+
+print('my name is '+ name + ' ' + surname+ 'i am ' + age + ' years old' )
---
+++ Day 04/Homework/task2.py
@@ -0,0 +1,7 @@
+"""2. დაწერეთ 5 არასწორი კოდი და კომენტარების მეშვეობით ახსენით, რატომ არის არასწორი."""
+
+1box="apple" #variable name cant start with number
+box 2="cake" #variable name cant contain space
+box3=(plant) #string should be enclosed in quotes
+box4 "tree" #missing equal sign
+print "tree" #missing parenthesis
