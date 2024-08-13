1fe726227a6a011c6cb2c9a7ba5c485bc17fae2d
teklamandaria <teklamandaria13@gmail.com>
2024-08-13 06:02:02 -0800

    commit

---
+++ Day 06/homework/task3.py
@@ -0,0 +1,7 @@
+#3. შექმენით ცვლადი სახელად user_name, სადაც მომხარებელს შემოატანინებთ სახელს, 
+# შემდეგ შექმენით ცვლადი სახელად last_name, სადაც მომხმარებელს შემოატანინებთ გვარს, 
+# ბოლოს დაუპრინტეთ მსგავსი მესიჯი: 'hello ' + (სახელი) + (გვარი).
+
+user_name=input("enter your name")
+last_name=input("enter your lastname")
+print("hello " + user_name + " " +last_name)
---
+++ Day 06/homework/task5.py
@@ -0,0 +1,23 @@
+#5. შექმენით ორი ცვლადი, სადაც მომხმარებელს შემოატანინებთ ორ რიცხვს, 
+# შეასრულეთ ამ რიცხვებზე შეკრება, გამოკლება, გამრავლება, გაყოფა და დაპრინტეთ შედეგები,
+#  (მინიშნება: input() მთლიანათ გადააქციეთ ინტეჯერად, int() ფუნქციის მეშვეობით).
+
+age=int(input("how old are you"))
+height=int(input("what hight are you"))
+
+print(age+height)
+print(age-height)
+print(age*height)
+print(age/height)
+
+
+
+
+
+
+
+
+
+
+
+
