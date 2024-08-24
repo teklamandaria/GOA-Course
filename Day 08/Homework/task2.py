7072b1e01a0c1c1fca353280222b47d6fdc23316
teklamandaria <teklamandaria13@gmail.com>
2024-08-24 09:43:24 -0800

    commit

---
+++ Day 08/Homework/task2.py
@@ -0,0 +1,9 @@
+#2. შექმენი 3 ცვლადი, სამივეში შეინახე განსხვავებული ინფორმაციის ტიპები, საბოლოოდ type() ფუნქციის გამოყენებით დაპრინტე მათი ტიპები.
+
+name="tekla"
+age=11
+student=True 
+
+print(type(name))
+print(type(age))
+print(type(student))
---
+++ Day 08/Homework/task3.py
@@ -0,0 +1,10 @@
+#3. მომხმარებელს Input()-ით შემოატანინე 2 რიცხვი და ამ ორ რიცხვზე გააკეთე გაყოფა, გამრავლება, მიმატება, გამოკლება
+
+num1=int(input("enter first number: "))
+num2=int(input("enter second number: "))
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
